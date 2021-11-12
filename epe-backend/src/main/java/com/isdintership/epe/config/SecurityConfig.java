e762bdbee022a2f2b79fe96fe8d433e20aeb61ea
ExxaD <max7gri@gmail.com>
2021-11-12 13:41:12 +0200

    Fixed JWT invalid/expired exception handling

--- epe-backend/src/main/java/com/isdintership/epe/config/SecurityConfig.java
+++ epe-backend/src/main/java/com/isdintership/epe/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.isdintership.epe.config;
 
 
+import com.isdintership.epe.security.ExceptionHandlerFilter;
 import com.isdintership.epe.security.jwt.JwtConfigurer;
 import com.isdintership.epe.security.jwt.JwtTokenProvider;
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,19 +13,22 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 @Configuration
 @EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     private final JwtTokenProvider jwtTokenProvider;
+    private ExceptionHandlerFilter exceptionHandlerFilter;
 
     private static final String ADMIN_ENDPOINT = "/api/admin/**";
     private static final String AUTHENTICATION_ENDPOINT = "/api/auth/**";
 
     @Autowired
-    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider, ExceptionHandlerFilter exceptionHandlerFilter) {
         this.jwtTokenProvider = jwtTokenProvider;
+        this.exceptionHandlerFilter = exceptionHandlerFilter;
     }
 
     @Override
@@ -45,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
+                .addFilterAfter(exceptionHandlerFilter, ChannelProcessingFilter.class)
                 .apply(new JwtConfigurer(jwtTokenProvider));
     }
 }
