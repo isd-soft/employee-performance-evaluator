cc9e44acd25e947143998191b248546dfd52fade
Timofei <timofei.isd.internship@gmail.com>
2021-11-15 12:03:27 +0200

    commit before pull

--- epe-frontend/src/app/app.module.ts
+++ epe-frontend/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import {TokenInterceptor} from "./interceptors/token.interceptor";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register-template/register.component';
 import { LoginComponent } from './login/login-template/login.component';
 import { TestComponent } from './test/test.component';
-import { HomeComponent } from './home/home-template/home.component';
 import { LogoutComponent } from './logout/logout.component';
 import { DashboardComponent } from './dashboard/dashboard-template/dashboard.component';
 
@@ -18,7 +18,6 @@ import { DashboardComponent } from './dashboard/dashboard-template/dashboard.com
     RegisterComponent,
     LoginComponent,
     TestComponent,
-    HomeComponent,
     LogoutComponent,
     DashboardComponent
   ],
@@ -28,7 +27,15 @@ import { DashboardComponent } from './dashboard/dashboard-template/dashboard.com
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true},
+    {provide: 'API_URL', useValue: getApiUrl()}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function getApiUrl(): string {
+  const baseHref = (document.querySelector('base') || {}).href;
+  return `${baseHref}api`;
+}
