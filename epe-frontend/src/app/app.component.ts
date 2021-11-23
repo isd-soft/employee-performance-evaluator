883bfcf00c9ba34872d7b8ad2d1db29b3064ba13
ExxaD <max7gri@gmail.com>
2021-11-23 20:48:51 +0200

    FR-5 Addedd ass. template button to sidebar

--- epe-frontend/src/app/app.component.ts
+++ epe-frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {JwtService} from "./decoder/decoder-service/jwt.service";
+import {JwtUser} from "./decoder/decoder-model/jwt-user.interface";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'epe';
+  jwtUser?: JwtUser;
+  isUser?: boolean = false;
+  isAdmin?: boolean = false;
+  isSysAdmin?: boolean = false;
+
+  constructor(private jwtService: JwtService) {
+
+    this.jwtUser = this.jwtService.getJwtUser();
+
+    if(this.jwtUser) {
+      if(this.jwtUser.role == 'ROLE_SYSADMIN') {
+        this.isSysAdmin = true;
+        this.isAdmin = true;
+        this.isUser = true;
+      }
+
+      if(this.jwtUser.role == 'ROLE_ADMIN') {
+        this.isAdmin = true;
+        this.isUser = true;
+      }
+
+      if(this.jwtUser.role == 'ROLE_USER') {
+        this.isUser = true;
+      }
+    }
+
+  }
+
 }
